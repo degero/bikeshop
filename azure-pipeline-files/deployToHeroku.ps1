9d9f560ca64221424c8a5cf3c6336ae3ce33f12b
Chad Paynter <chad.paynter@southlink.co.nz>
2020-05-09 13:09:15 +1000

    Fix devops pipeline directory clean

--- azure-pipeline-files/deployToHeroku.ps1
+++ azure-pipeline-files/deployToHeroku.ps1
@@ -15,7 +15,7 @@ xcopy ..\server.js . /Y
 xcopy ..\db.json . /Y
 xcopy ..\Procfile . /Y
 git add .
-Write-Host "Setting git $Env:GIT_NAME"
-git config user.name $Env:GIT_NAME
-git config user.email $Env:GIT_EMAIL
-git commit -m "Azure devops deploy"
+Write-Host "Commit and Push to heroku"
+$GitDetails = $Env:GIT_NAME + " <" + $Env:GIT_EMAIL + ">"
+git commit -m "Azure devops deploy" --author=$GitDetails
+git push
