175d00fcf9739c7c76562b8ae0b7637b479e9899
Chad Paynter <chad.paynter@southlink.co.nz>
2020-05-09 13:59:08 +1000

    Added tests using react-testing-library

--- src/components/bikes/BikeForm.js
+++ src/components/bikes/BikeForm.js
@@ -1,47 +1,45 @@
 import React from "react";
 import TextInput from "../common/TextInput";
 
-const BikeForm = ({ bike, onSave, onChange, errors = {}, saving = false }) => {
-  return (
-    <div className="row content">
-      <div className="col-12 col-md-4">
-        <h2>{bike.id ? "Edit" : "Create"} Bike</h2>
-        {errors.onSave && (
-          <div className="alert alert-danger" role="alert">
-            {errors.onSave}
-          </div>
-        )}
-        <form onSubmit={onSave} autoComplete="off">
-          <TextInput
-            name="manufacturer"
-            label="Manufacturer:"
-            onChange={onChange}
-            value={bike.manufacturer}
-            error={errors.manufacturer}
-          ></TextInput>
-          <TextInput
-            name="model"
-            label="Model:"
-            onChange={onChange}
-            value={bike.model}
-            error={errors.model}
-          ></TextInput>
-          <TextInput
-            name="price"
-            label="Price ($USD):"
-            onChange={onChange}
-            value={bike.price}
-            error={errors.price}
-          ></TextInput>
-          <input
-            className="btn btn-primary"
-            type="submit"
-            value={saving ? "Saving" : "Save"}
-          />
-        </form>
-      </div>
+const BikeForm = ({ bike, onSave, onChange, errors = {}, saving = false }) => (
+  <div className="row content">
+    <div className="col-12 col-md-4">
+      <h2>{bike.id ? "Edit" : "Create"} Bike</h2>
+      {errors.onSave && (
+        <div className="alert alert-danger" role="alert">
+          {errors.onSave}
+        </div>
+      )}
+      <form onSubmit={onSave} autoComplete="off">
+        <TextInput
+          name="manufacturer"
+          label="Manufacturer:"
+          onChange={onChange}
+          value={bike.manufacturer}
+          error={errors.manufacturer}
+        ></TextInput>
+        <TextInput
+          name="model"
+          label="Model:"
+          onChange={onChange}
+          value={bike.model}
+          error={errors.model}
+        ></TextInput>
+        <TextInput
+          name="price"
+          label="Price ($USD):"
+          onChange={onChange}
+          value={bike.price}
+          error={errors.price}
+        ></TextInput>
+        <input
+          className="btn btn-primary"
+          type="submit"
+          value={saving ? "Saving" : "Save"}
+        />
+      </form>
     </div>
-  );
-};
+  </div>
+);
 
 export default BikeForm;
---
+++ src/components/bikes/BikeForm.test.js
@@ -0,0 +1,55 @@
+import React from "react";
+import { cleanup, render } from "@testing-library/react";
+import BikeForm from "./BikeForm";
+import { newBike } from "../../models/bike";
+
+afterEach(cleanup);
+
+describe("BikeForm component", () => {
+  it("should render Create bike header when bike prop is empty", () => {
+    // ARRANGE / ACT
+    const { getByText } = renderBikeForm();
+    // ASSERT
+    getByText("Create Bike");
+  });
+
+  it("should render Edit bike header when bike prop has id", () => {
+    // ARRANGE / ACT
+    let bike = newBike;
+    bike.id = 22;
+    const { getByText } = renderBikeForm({ bike });
+    // ASSERT
+    getByText("Edit Bike");
+  });
+
+  it('should label save button as "Save" when not saving', () => {
+    // ARRANGE / ACT
+    const { getByText } = renderBikeForm();
+    // ASSERT
+    getByText("Save");
+  });
+
+  it('should label save button as "Saving..." when saving', () => {
+    // ARRANGE / ACT
+    const { getByText } = renderBikeForm({ saving: true });
+    // ASSERT
+    getByText("Saving");
+  });
+});
+
+//#region helper functions
+
+function renderBikeForm(args) {
+  let defaultProps = {
+    bike: newBike,
+    onSave: () => {},
+    onChange: () => {},
+    errors: {},
+    saving: false,
+  };
+
+  const props = { ...defaultProps, ...args };
+  return render(<BikeForm {...props} />);
+}
+
+//#endregion
