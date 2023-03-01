c78ddd8214fb062dd2fad3a878e59f7b311d1f08
degero <chaddotnet@outlook.com>
2023-03-01 16:26:17 +0700

    add google analtyics

--- src/index.js
+++ src/index.js
@@ -5,7 +5,11 @@ import App from "./components/App";
 import configureStore from "./redux/store/configureStore";
 import { Provider as ReduxProvider } from "react-redux";
 import "bootstrap/dist/css/bootstrap.css";
-
+import TagManager from 'react-gtm-module'
+const tagManagerArgs = {
+  gtmId: process.env.REACT_APP_GANALYTICS_TAG_ID
+}
+TagManager.initialize(tagManagerArgs)
 const store = configureStore();
 
 ReactDOM.render(
